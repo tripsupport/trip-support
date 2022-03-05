526e6ee9fa84b5667dd26635b784605f149f54f1
Arif Alim <49820680+arif-alim@users.noreply.github.com>
2022-03-05 14:45:34 -0500

    made changes to svelte file

---
+++ .vercel_build_output/static/_app/pages/flight-hotel.svelte-d0cb0f26.js
@@ -0,0 +1,1 @@
+import{S as s,i as e,s as l}from"../chunks/vendor-f9ddc6f7.js";class n extends s{constructor(t){super();e(this,t,null,null,l,{})}}export{n as default};
--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,23 @@
 // import adapter from '@sveltejs/adapter-static';
-import vercel from '@sveltejs/adapter-vercel';
 
-export default {
-	kit: {
-		adapter: vercel({
-			// default options are shown
-			// pages: 'build',
-			// assets: 'build',
-			// fallback: null,
-			// precompress: false
-		})
-	}
-};
-// import adapter from '@sveltejs/adapter-auto';
-
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
+// export default {
 // 	kit: {
-// 		adapter: vercel()
+// 		adapter: adapter({
+// 			pages: 'build',
+// 			assets: 'build',
+// 			fallback: null,
+// 			precompress: false
+// 		})
 // 	}
 // };
+// import adapter from '@sveltejs/adapter-auto';
 
-// export default config;
+import vercel from '@sveltejs/adapter-vercel';
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: vercel()
+	}
+};
+
+export default config;
